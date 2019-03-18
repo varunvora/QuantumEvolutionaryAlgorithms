2f82d6522c8baffba82469d5b7546956e713d433
Krishna Kumar P,Vishal <Vishal.KrishnaKumarP@Cerner.com>
2019-03-19 01:35:39 +0530

    loaded data and displayed iris and wine with iris plot

---
+++ datasets/dataset_loader.py
@@ -0,0 +1,8 @@
+from sklearn import datasets
+import matplotlib.pyplot as plt
+
+
+#loading iris dataset
+iris = datasets.load_iris(return_X_y = False)
+#loading wine dataset
+wine = datasets.load_wine(return_X_y = False)
