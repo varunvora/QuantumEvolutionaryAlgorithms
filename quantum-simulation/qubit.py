16c94142d263204cc15d0268aa6cc32c84b442f8
Varun Vora <varun.vora@gonuclei.com>
2019-04-27 02:49:39 +0530

    changes for report

--- quantum-simulation/qubit.py
+++ quantum-simulation/qubit.py
@@ -4,6 +4,7 @@ from pprint import pprint
 from logging import error
 import numpy as np
 
+
 class Qubit:
     def __init__(self):
         self.measured_value = None
@@ -13,16 +14,6 @@ class Qubit:
 
     def __str__(self):
         return f'{self.a}|0> + {self.b}|1>'
-
-    def __setattr__(self, key, value):
-        try:
-            pprint(vars(self))
-            if self.is_measured():
-                error(f'Can not set {value} for {key} after qubit is measured')
-                return
-            super(Qubit, self).__setattr__(key, value)
-        except AttributeError:
-            super(Qubit, self).__setattr__(key, value)
 
     def is_measured(self) -> bool:
         return self.measured_value is not None
@@ -34,7 +25,6 @@ class Qubit:
         return self.measured_value
 
     # defining single qubit logic gates
-
     def hadamard_gate(self):
         self.a, self.b = (self.a + self.b) / sqrt(2), (self.a - self.b) / sqrt(2)
 
@@ -49,7 +39,7 @@ class Qubit:
         """
         It equates to a rotation around the Y-axis of the Bloch sphere by pi radians.
         """
-        # todo
+        self.a, self.b = 1j * self.b, -1j * self.a
         pass
 
     def pauli_z_gate(self):
@@ -67,9 +57,9 @@ class Qubit:
         bNOT = not self.b
         if (isGreater):
             if not ri and bi:
-                dt = np.pi * .05 #angle of rotation
+                dt = np.pi * .05  # angle of rotation
                 if aNOT:
-                    sign = 1 #sign of the rotation
+                    sign = 1  # sign of the rotation
                 elif bNOT:
                     sign = 0
                 elif positive:
@@ -118,15 +108,11 @@ class Qubit:
                 else:
                     sign = -1
 
-        t = sign * dt #product of angle with the sign
-        #applying the rotation gate as a dot product
+        t = sign * dt  # product of angle with the sign
+        # applying the rotation gate as a dot product
         self.a, self.b = np.dot(
             np.array([[np.cos(t), -np.sin(t)], [np.sin(t), np.cos(t)]]), np.array([self.a, self.b])
         )
-
-
-
-
 
 
 if __name__ == "__main__":
